f83678f8e0687e7b79edf371a7865e0929cd7ff3
jimao <1982775886@qq.com>
2021-06-02 19:41:12 +0800

    代码排版

--- contracts/LiquidityValueCalculator.sol
+++ contracts/LiquidityValueCalculator.sol
@@ -20,10 +20,14 @@ contract LiquidityValueCalculator is ILiquidityValueCalculator {
     uint reserveB,
     uint totalSupply
   ) {
-    IUniswapV2Pair pair = IUniswapV2Pair(UniswapV2Library.pairFor(factory, tokenA, tokenB));
+    IUniswapV2Pair pair = IUniswapV2Pair(
+      UniswapV2Library.pairFor(factory, tokenA, tokenB)
+    );
     totalSupply = pair.totalSupply();
     (uint reserves0, uint reserves1,) = pair.getReserves();
-    (reserveA, reserveB) = tokenA == pair.token0() ? (reserves0, reserves1) : (reserves1, reserves0);
+    (reserveA, reserveB) = tokenA == pair.token0() ?
+      (reserves0, reserves1) :
+      (reserves1, reserves0);
   }
 
   function computeLiquidityShareValue(
