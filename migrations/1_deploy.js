249fcace6c65d7b09fd1703a76b98611002e2452
jimao <1982775886@qq.com>
2021-06-02 19:43:16 +0800

    修改部署脚本

--- migrations/1_deploy.js
+++ migrations/1_deploy.js
@@ -1,7 +1,9 @@
 const TestCoin1 = artifacts.require('TestCoin1');
 const TestCoin2 = artifacts.require('TestCoin2');
+const LiquidityValueCalculator = artifacts.require('LiquidityValueCalculator');
 
 module.exports = (deployer) => {
-  deployer.deploy(TestCoin1);
-  deployer.deploy(TestCoin2);
+  // deployer.deploy(TestCoin1);
+  // deployer.deploy(TestCoin2);
+  deployer.deploy(LiquidityValueCalculator, '0xE76Ac258Ca830E31bCB973D6CbeFeB4901360eBF');
 };
