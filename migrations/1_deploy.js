274845ab5213ce69db8c3006a3036f43108d7bad
jimao <1982775886@qq.com>
2021-06-11 23:32:40 +0800

    部署代币

--- migrations/1_deploy.js
+++ migrations/1_deploy.js
@@ -3,7 +3,7 @@ const TestCoin2 = artifacts.require('TestCoin2');
 const LiquidityValueCalculator = artifacts.require('LiquidityValueCalculator');
 
 module.exports = (deployer) => {
-  // deployer.deploy(TestCoin1);
-  // deployer.deploy(TestCoin2);
-  deployer.deploy(LiquidityValueCalculator, '0xE76Ac258Ca830E31bCB973D6CbeFeB4901360eBF');
+  deployer.deploy(TestCoin1);
+  deployer.deploy(TestCoin2);
+  // deployer.deploy(LiquidityValueCalculator, '0xE76Ac258Ca830E31bCB973D6CbeFeB4901360eBF');
 };
